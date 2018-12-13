5ac604ee87b3a6d7d6f3f7b140625a81d7fbc133
Alex Shvalov <uzzzer@gmail.com>
2018-12-13 22:06:29 +0300

    modified a bit

--- game.py
+++ game.py
@@ -6,6 +6,7 @@ window = turtle.Screen()
 window.setup(1300, 900)
 window.bgpic("images/background.png")
 window.screensize(1200,800)
+window.tracer(n=2)
 
 BASE_X, BASE_Y = 0, -350
 
@@ -18,23 +19,47 @@ def calc_heading(x1, y1, x2, y2):
     return alpha
 
 def fire_missile(x, y):
+    global missile
     missile = turtle.Turtle()
+    missile.speed(0)
     missile.color("white")
-    missile.penup()
-    missile.setpos(x = BASE_X, y= BASE_Y)
-    heading = calc_heading(x1=BASE_X, y1=BASE_Y, x2=x, y2=y)
-    missile.pendown()
-    missile.setheading(heading)
-    missile.forward(500)
-    missile.shape("circle")
-    missile.shapesize(1)
-    missile.shapesize(4)
-    missile.shapesize(6)
-    missile.shapesize(8)
-    missile.shapesize(10)
-    missile.clear()
     missile.hideturtle()
+    missile.setpos(x = BASE_X, y= BASE_Y)
+    missile.pendown()
+    heading = calc_heading(x1=BASE_X, y1=BASE_Y, x2=x, y2=y)
+    missile.setheading(heading)
+    missile.showturtle()
+    # missile.forward(500)
+    # missile.shape("circle")
+    # missile.shapesize(1)
+    # missile.shapesize(4)
+    # missile.shapesize(6)
+    # missile.shapesize(8)
+    # missile.shapesize(10)
+    # missile.clear()
+    # missile.hideturtle()
+    our_missiles.append(missile)
+    our_missiles_target.append([x, y])
 
 window.onclick(fire_missile)
 
-window.mainloop()
+missile = None
+
+our_missiles = []
+our_missiles_target = []
+
+
+while True:
+    window.update()
+
+    for missile in our_missiles:
+        missile.forward(4)
+        target = our_missiles_target[num]
+        if missile.distance(x=target[0], y=target[1]) < 20:
+            missile.shape(circle)
+            missile.shapesize(2)
+            missile.shapesize(4)
+            missile.shapesize(6)
+            
+
+#window.mainloop()
