6914e9ed768a7639f32e9a120b653aeea5aec72c
Alex Shvalov <uzzzer@gmail.com>
2018-12-13 22:14:53 +0300

    added original source code

--- game_v2.py
+++ game_v2.py
@@ -1,14 +1,14 @@
-import turtle
-import random
 import math
+import turtle
 
 window = turtle.Screen()
-window.setup(1300, 900)
+window.setup(1200 + 3, 800 + 3)
 window.bgpic("images/background.png")
-window.screensize(1200,800)
-window.tracer(n=2)
+window.screensize(1200, 800)
+# window.tracer(n=2)
 
-BASE_X, BASE_Y = 0, -350
+BASE_X, BASE_Y = 0, -300
+
 
 def calc_heading(x1, y1, x2, y2):
     dx = x2 - x1
@@ -18,48 +18,44 @@ def calc_heading(x1, y1, x2, y2):
     alpha = math.degrees(alpha)
     return alpha
 
+
 def fire_missile(x, y):
-    global missile
-    missile = turtle.Turtle(visible=False)
-    missile.speed(0)
-    missile.color("white")
+    missile = turtle.Turtle()
     missile.hideturtle()
-    missile.setpos(x = BASE_X, y= BASE_Y)
+    missile.speed(0)
+    missile.color('white')
+    missile.penup()
+    missile.setpos(x=BASE_X, y=BASE_Y)
     missile.pendown()
     heading = calc_heading(x1=BASE_X, y1=BASE_Y, x2=x, y2=y)
     missile.setheading(heading)
     missile.showturtle()
     # missile.forward(500)
-    # missile.shape("circle")
-    # missile.shapesize(1)
+    # missile.shape('circle')
+    # missile.shapesize(2)
+    # missile.shapesize(3)
     # missile.shapesize(4)
-    # missile.shapesize(6)
-    # missile.shapesize(8)
-    # missile.shapesize(10)
+    # missile.shapesize(5)
     # missile.clear()
     # missile.hideturtle()
     our_missiles.append(missile)
     our_missiles_target.append([x, y])
 
-window.onclick(fire_missile)
 
-missile = None
+window.onclick(fire_missile)
 
 our_missiles = []
 our_missiles_target = []
 
-
 while True:
     window.update()
 
-    for missile in our_missiles:
+    for num, missile in enumerate(our_missiles):
         missile.forward(4)
         target = our_missiles_target[num]
         if missile.distance(x=target[0], y=target[1]) < 20:
-            missile.shape("circle")
+            missile.shape('circle')
             missile.shapesize(2)
+            missile.shapesize(3)
             missile.shapesize(4)
-            missile.shapesize(6)
-
-
-#window.mainloop()
+            missile.shapesize(5)
